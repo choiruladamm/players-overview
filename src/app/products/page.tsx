985c64690f0380855b008d493c494c6d820b74c5
choiruladamm <choiruladamm@gmail.com>
2025-11-11 20:29:58 +0700

    chore: rm unused boilerplate starter

--- src/app/products/page.tsx
+++
@@ -1,8 +0,0 @@
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-	title: 'Products Page - Next.js Starter',
-	description: 'This is example of products page',
-};
-
-export { default } from '@/features/products/pages/product-page';
